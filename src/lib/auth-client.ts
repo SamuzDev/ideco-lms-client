c1a07394bb14f043cd411b92c809629707f01287
Samuz <deyverabanto@gmail.com>
2025-05-12 00:53:54 -0500

    Change Env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { twoFactorClient } from "better-auth/client/plugins"
 
 export const { useSession, signIn, signOut, signUp, getSession, resetPassword, forgetPassword, twoFactor, listAccounts } = createAuthClient({
-    baseURL: `${import.meta.env.VITE_URL_SERVER as string}`,
+    baseURL: import.meta.env.VITE_URL_SERVER as string,
     redirectTo: "/",
     plugins: [
         twoFactorClient()
